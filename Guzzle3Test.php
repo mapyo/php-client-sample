b1d8486c2d472444ed12b41ee9c9327aff30f734
mapyo <falcon.phantom.spidar.haptic@gmail.com>
2014-11-01 23:55:50 +0900

    Resource Owner Password Credentials Grant で tokenを取得する

--- Guzzle3Test.php
+++ Guzzle3Test.php
@@ -1,11 +1,23 @@
 <?php
-require_once 'config/config.php';
 require 'vendor/autoload.php';
 
 use Guzzle\Http\Client;
+use CommerceGuys\Guzzle\Plugin\Oauth2\GrantType\PasswordCredentials;
 
-$confing = CONFIG::$default;
+$settings = parse_ini_file('config/settings.ini');
 
 # Resource Owner Password Credentials Grant
-# http://oauthlib.readthedocs.org/en/latest/oauth2/grants/password.html#resource-owner-password-credentials-grant
-$client = new Client($confing['base_url'] . '/oauth/token')
+$oauth2Client = new Client($settings['base_url'] . '/oauth/token');
+$config = array(
+    'username'      => $settings['username'],
+    'password'      => $settings['password'],
+    'client_id'     => $settings['client_id'],
+    'client_secret' => $settings['client_secret'],
+    // 'client_id'     => null, # null でもよい
+    // 'client_secret' => null, # null でもよい
+    'scope'         => $settings['scope'], // Optional.
+);
+
+$grantType = new PasswordCredentials($oauth2Client, $config);
+$tokenData = $grantType->getTokenData();
+var_dump($tokenData['access_token']);
