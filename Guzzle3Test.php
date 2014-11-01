95a1fbe2521217f98d681cb69234644911e097ad
mapyo <falcon.phantom.spidar.haptic@gmail.com>
2014-11-02 01:42:35 +0900

    プラグインを使わないように書き換えた。

--- Guzzle3Test.php
+++ Guzzle3Test.php
@@ -2,22 +2,17 @@
 require 'vendor/autoload.php';
 
 use Guzzle\Http\Client;
-use CommerceGuys\Guzzle\Plugin\Oauth2\GrantType\PasswordCredentials;
 
 $settings = parse_ini_file('config/settings.ini');
 
 # Resource Owner Password Credentials Grant
-$oauth2Client = new Client($settings['base_url'] . '/oauth/token');
-$config = array(
+$client = new Client($settings['base_url']);
+$params = array(
+    'grant_type'    => 'password',
     'username'      => $settings['username'],
     'password'      => $settings['password'],
-    'client_id'     => $settings['client_id'],
-    'client_secret' => $settings['client_secret'],
-    // 'client_id'     => null, # null でもよい
-    // 'client_secret' => null, # null でもよい
     'scope'         => $settings['scope'], // Optional.
 );
 
-$grantType = new PasswordCredentials($oauth2Client, $config);
-$tokenData = $grantType->getTokenData();
-var_dump($tokenData['access_token']);
+$data = $client->post('/oauth/token', array(), $params)->send()->json();
+var_dump($data['access_token']);
