14b00e28ea04538d73fa5876f081ca18d8243c2b
mapyo <falcon.phantom.spidar.haptic@gmail.com>
2014-11-10 08:59:47 +0900

    add fail test

--- guzzle-active-record-sample/tests/QueryingTest.php
+++ guzzle-active-record-sample/tests/QueryingTest.php
@@ -5,8 +5,9 @@ class QueryingTest extends PHPUnit_Framework_TestCase
 {
     public function setUp()
     {
-        $connection = m::mock('GuzzleActiveSample\Connection');
-        $this->model = new QueryModelStub($connection);
+        $this->connection = m::mock('GuzzleActiveSample\Connection');
+        $this->message = m::mock('Guzzle\Http\Message\Response');
+        $this->model = new QueryModelStub($this->connection);
     }
 
     public function testTheSingularQueryableName()
@@ -17,6 +18,28 @@ class QueryingTest extends PHPUnit_Framework_TestCase
     public function testThePluralQueryableName()
     {
         $this->assertEquals('the_plural_name', $this->model->queryableOptions()->plural());
+    }
+
+    public function testFindOneReturnsOneEntry()
+    {
+        $stub = file_get_contents(dirname(__FILE__) . '/stubs/stub.json');
+        $this->message->shouldReceive('json')->andReturn(json_encode($stub, true));
+        $this->connection->shouldReceive('get')->andReturn($this->message);
+
+        $response = $this->model->find(1);
+
+        $this->assertTrue(isset($response['stub']));
+    }
+
+    public function testFindAllReturnsAllEntities()
+    {
+        $stub = file_get_contents(dirname(__FILE__) . '/stubs/stub.json');
+        $this->message->shouldReceive('json')->andReturn(json_encode($stub, true));
+        $this->connection->shouldReceive('get')->andReturn($this->message);
+
+        $response = $this->model->all();
+
+        $this->assertTrue(isset($response['stub']));
     }
 }
 
