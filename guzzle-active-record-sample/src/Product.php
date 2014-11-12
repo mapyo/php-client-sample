b78fb9096c43213f3cb8f88388901c9489703380
mapyo <falcon.phantom.spidar.haptic@gmail.com>
2014-11-12 11:57:57 +0900

    一旦コミット

--- guzzle-active-record-sample/src/Product.php
+++ guzzle-active-record-sample/src/Product.php
@@ -3,6 +3,11 @@ namespace GuzzleActiveSample;
 
 class Product extends Model
 {
+
+    protected $fillable = array(
+        '' => '',
+        );
+
     public function __construct(Connection $connection)
     {
         parent::__construct($connection);
