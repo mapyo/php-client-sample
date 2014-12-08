8099e3c9ce8dae5b3846ca142274e9c7752aa082
mapyo <falcon.phantom.spidar.haptic@gmail.com>
2014-12-08 09:29:59 +0900

    1メソッドのテストが通ったところで一旦コミット

--- guzzle-active-record-sample/tests/PartyTest.php
+++ guzzle-active-record-sample/tests/PartyTest.php
@@ -24,6 +24,13 @@ class PartyTest extends PHPUnit_Framework_TestCase
         $this->connection->shouldReceive('get')->andReturn($this->message);
 
         $party = $this->party->find(100);
+
         $this->assertInstanceOf('GuzzleActiveSample\Person', $party);
+        $this->assertEquals('100', $party->id);
+        $this->assertEquals('Eric', $party->first_name);
+        $this->assertEquals('Schmidt', $party->last_name);
+        $this->assertEquals('2011-09-14T15:22:01Z', $party->created_on);
+        $this->assertEquals('2011-12-14T10:45:46Z', $party->updated_on);
     }
+
 }
